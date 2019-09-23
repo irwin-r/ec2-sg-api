0fbf101ffb4d26812b34d636190cf152ca68ae0f
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2019-09-23 23:14:37 +1000

    Created initial list (GET) endpoint.

--- src/handlers/list.js
+++ src/handlers/list.js
@@ -1,6 +1,22 @@
-module.exports = async (event) => ({
-  statusCode: 200,
-  body: JSON.stringify({
-    message: `Hello, the current time is ${new Date().toTimeString()}.`,
-  }),
-});
+const middy = require("middy");
+const status = require("http-status");
+
+const { getSecurityGroups } = require("../aws");
+const { errorMiddleware } = require("../middlewares");
+const { SecurityGroupSerializer } = require("../serializers");
+const { InternalServerError } = require("../utils/errors");
+
+const list = async () => {
+  const securityGroups = await getSecurityGroups();
+
+  if (Array.isArray(securityGroups)) {
+    throw new InternalServerError("Unable to retrieve security groups.");
+  }
+
+  return {
+    statusCode: status.OK,
+    body: JSON.stringify(SecurityGroupSerializer.serialize(securityGroups)),
+  };
+};
+
+module.exports = middy(list).use(errorMiddleware());
