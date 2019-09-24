fe9b774482f887c87b01f51c3698329dba8cf157
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2019-09-24 17:50:35 +1000

    Slightly refactored authorizer to improve legibility.

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,16 +1,10 @@
 const status = require("http-status");
 
 class InternalServerError extends Error {
-  constructor(message, ...rest) {
-    super(message, ...rest);
-
-    this.detail =
-      typeof message === "string"
-        ? message
-        : status[`${status.INTERNAL_SERVER_ERROR}_MESSAGE`];
-
-    this.message = status[status.INTERNAL_SERVER_ERROR];
+  constructor(detail = status[`${status.INTERNAL_SERVER_ERROR}_MESSAGE`]) {
+    super(status[status.INTERNAL_SERVER_ERROR]);
     this.statusCode = status.INTERNAL_SERVER_ERROR;
+    this.detail = detail;
   }
 }
 
