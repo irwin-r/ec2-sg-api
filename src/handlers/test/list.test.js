aa6c79bfc5e685fd09de8b13d893847d350620cf
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2019-09-24 18:17:25 +1000

    Fixed failing test.

--- src/handlers/test/list.test.js
+++ src/handlers/test/list.test.js
@@ -1,9 +1,11 @@
 const { expect } = require("chai");
 
-const list = require("../list");
+const { list } = require("../index");
 
 describe("GET - List Endpoint", () => {
   it("should return successfully", async () => {
-    expect((await list()).statusCode).to.equal(200);
+    list(null, null, res => {
+      expect(res.statusCode).to.equal(200);
+    });
   });
 });
