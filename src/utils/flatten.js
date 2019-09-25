c35b0f349601abed8c56ec96f496936eb28c9951
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2019-09-25 21:08:11 +1000

    Removed methods now obsolete by lodash usage.

--- src/utils/flatten.js
+++
@@ -1,4 +0,0 @@
-// We'd really rather be using Array.prototype.flat but Node 10.x doesn't support it and we're not using babel/polyfills
-const flatten = arrays => arrays.reduce((a, b) => [...a, ...b], []);
-
-module.exports = flatten;
