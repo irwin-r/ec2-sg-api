275f055e0551770eba88d5b717d9a04559aa920c
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2019-09-26 16:47:42 +1000

    Created unit tests for authorization token validator method.

---
+++ src/utils/tests/validateAuthorizationToken.test.js
@@ -0,0 +1,54 @@
+const { expect } = require("chai");
+const faker = require("faker");
+const jwt = require("jsonwebtoken");
+
+const validateAuthorizationToken = require("../validateAuthorizationToken");
+
+describe("validateAuthorizationToken", () => {
+  describe("when a valid JWT is passed", () => {
+    it("should return the decrypted JWT", () => {
+      const key = faker.internet.password();
+      const body = { [faker.random.word()]: faker.random.words() };
+      const token = jwt.sign(body, key, { expiresIn: "1y" });
+      const authorizationToken = `Bearer ${token}`;
+
+      const validated = validateAuthorizationToken(authorizationToken, key);
+
+      expect(validated).to.not.equal(false);
+      expect(validated).to.own.include(body);
+    });
+  });
+
+  describe("when an expired JWT is passed", () => {
+    it("should return false", () => {
+      const key = faker.internet.password();
+      const token = jwt.sign({ exp: Math.floor(Date.now() / 1000) - 60 * 60 }, key);
+      const authorizationToken = `Bearer ${token}`;
+
+      const validated = validateAuthorizationToken(authorizationToken, key);
+
+      expect(validated).to.equal(false);
+    });
+  });
+
+  describe("when an invalid JWT is passed", () => {
+    it("should return false", () => {
+      const authorizationToken = `Bearer ${faker.random.words()}`;
+      const key = faker.internet.password();
+
+      const validated = validateAuthorizationToken(authorizationToken, key);
+
+      expect(validated).to.equal(false);
+    });
+  });
+
+  describe("when nothing is passed at all", () => {
+    it("should return false", () => {
+      const key = faker.internet.password();
+
+      const validated = validateAuthorizationToken("", key);
+
+      expect(validated).to.equal(false);
+    });
+  });
+});
